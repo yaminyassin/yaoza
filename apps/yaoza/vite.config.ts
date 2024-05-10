f3e07a4b77752ba96e5ef5d030184588d4bbcf26
Yamin Yassin <x210621@bcpcorp.net>
2024-05-10 23:39:13 +0100

    builds & serves

--- apps/yaoza/vite.config.ts
+++ apps/yaoza/vite.config.ts
@@ -2,9 +2,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import path from 'node:path';
 
+const resolve = (aliasName: string, modulePath: string) => {
+  return {
+    find: aliasName,
+    replacement: path.resolve(__dirname, modulePath),
+  };
+};
+
+const resolveAccolade = (aliasName: string, modulename: string) => {
+  return resolve(aliasName, `../../../packages/${modulename}`);
+};
 export default defineConfig({
   root: __dirname,
+  //TODO: Edit Dir when changing app name
   cacheDir: '../../node_modules/.vite/apps/yaoza',
 
   server: {
@@ -18,11 +30,30 @@ export default defineConfig({
   },
 
   plugins: [react(), nxViteTsPaths()],
+  //TODO: resolve this
+  resolve: {
+    alias: [
+      resolveAccolade('@bcp-nextgen-dx-component-factory/design-system', 'design-system/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/theme-accolade', 'theme-accolade/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/theme-foundation', 'theme-foundation/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/atoms', 'atoms/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/base', 'base/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/core', 'core/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/docs', 'docs/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/icons-accolade', 'icons-accolade/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/icons-foundation', 'icons-foundation/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/molecules', 'molecules/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/organisms', 'organisms/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/utilities', 'utilities/src'),
+      resolveAccolade('@bcp-nextgen-dx-component-factory/storybook-template', 'storybook-template/src'),
+    ],
+  },
 
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
+  
 
   build: {
     outDir: '../../dist/apps/yaoza',
@@ -32,18 +63,18 @@ export default defineConfig({
     },
   },
 
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  // test: {
+  //   globals: true,
+  //   cache: {
+  //     dir: '../../node_modules/.vitest',
+  //   },
+  //   environment: 'jsdom',
+  //   include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/yaoza',
-      provider: 'v8',
-    },
-  },
+  //   reporters: ['default'],
+  //   coverage: {
+  //     reportsDirectory: '../../coverage/apps/yaoza',
+  //     provider: 'v8',
+  //   },
+  // },
 });
